20bc604ada6b8dae93d8b0c7fbbc0ad5188ef385
Peixee <Jc_px@hotmail.com>
2023-06-05 01:20:29 -0300

    Smart Bank - Utilizando StyledComponents
    Curso da Alura de estilização do React usando styled Components via react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { ThemeProvider } from "styled-components";
 
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
+import { GlobalStyle } from "./Components/GlobalStyle";
+import { temaClaro, temaEscuro } from "./Components/UI/temas";
+import { BtnTema } from "./Components/UI";
+import  SwitcherTema  from "./Components/SwitcherTheme/Index";
 
 function App() {
+  const [tema, setTema] = useState(true);
+
+  const toggleTema = () => {
+    setTema((tema) => !tema);
+  };
   return (
-    <>
+    <ThemeProvider theme={tema?temaClaro:temaEscuro}>
+
+      <GlobalStyle/>
+      <BtnTema onClick={toggleTema}>
+        <SwitcherTema theme={tema} />
+      </BtnTema>
       <Cabecalho />
       <Container />
-    </>
+    </ThemeProvider>
   );
 }
 
